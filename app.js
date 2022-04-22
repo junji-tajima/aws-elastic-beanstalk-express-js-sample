bf06182890d0bd36f9f1145284ddea941f516b45
unknown <jtajima@pc-daiwabo.co.jp>
2022-04-22 15:19:05 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('nice shot!!'));
+app.get('/', (req, res) => res.send('Test new commit to repository!!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
